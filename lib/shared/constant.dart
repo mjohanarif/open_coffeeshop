a867ccf507ec9b9c962641f421820766c4470594
mjohanarif <m.johanarifin@gmail.com>
2024-09-30 21:29:25 +0700

    chore setup locator

--- lib/shared/constant.dart
+++ lib/shared/constant.dart
@@ -1,4 +1,6 @@
 class Constant {
   static const String imagePlaceholderUrl =
       'https://img.freepik.com/free-photo/image-icon-front-side-white-background_187299-40166.jpg';
+  static const String apiUrl =
+      'https://api.open-meteo.com/v1/forecast?daily=weather_code&timezone=auto&forecast_days=3';
 }
